76d7ac612a13774370d85394339bab2d7f646441
Anil Polineni <anilpolineni@Anils-MacBook-Air.local>
2024-02-12 15:52:00 +0530

    updated form

--- src/Login.js
+++ src/Login.js
@@ -1,36 +1,41 @@
 // App.js
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import LoginForm from './LoginForm';
 
-function App() {
+function Login() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [token, setToken] = useState(localStorage.getItem('token') || null);
 
   useEffect(() => {
     const refreshToken = localStorage.getItem('refreshToken');
     if (refreshToken) {
-      axios.post('/api/refresh_token', { refreshToken })
-        .then(response => {
-          const { token } = response.data;
-          setToken(token);
-          setLoggedIn(true);
-          localStorage.setItem('token', token); // Store token in local storage
-        })
-        .catch(error => console.error(error));
+    //   axios.post('/api/refresh_token', { refreshToken })
+    //     .then(response => {
+    //       const { token } = response.data;
+    //       setToken(token);
+    //       setLoggedIn(true);
+    //       localStorage.setItem('token', token); // Store token in local storage
+    //     })
+    //     .catch(error => console.error(error));
     }
   }, []);
 
   const handleLogin = () => {
-    axios.post('/api/login', { username: 'username', password: 'password' })
-      .then(response => {
-        const { token, refreshToken } = response.data;
+    // axios.post('/api/login', { username: 'username', password: 'password' })
+    //   .then(response => {
+    //     const { token, refreshToken } = response.data;
+    //     localStorage.setItem('token', token); // Store token in local storage
+    //     localStorage.setItem('refreshToken', refreshToken); // Store refresh token in local storage
+    //     setToken(token);
+    //     setLoggedIn(true);
+    //   })
+    //   .catch(error => console.error(error));
+
+        const token='xyz';
         localStorage.setItem('token', token); // Store token in local storage
-        localStorage.setItem('refreshToken', refreshToken); // Store refresh token in local storage
+        localStorage.setItem('refreshToken', 'abc-refresh'); // Store refresh token in local storage
         setToken(token);
         setLoggedIn(true);
-      })
-      .catch(error => console.error(error));
   };
 
   const handleLogout = () => {
@@ -51,11 +56,11 @@ function App() {
         <div>
           <h1>Please log in.</h1>
           <LoginForm handleLogin={handleLogin} />
-          <button onClick={handleLogin}>Login</button>
+          {/* <button onClick={handleLogin}>Login</button> */}
         </div>
       )}
     </div>
   );
 }
 
-export default App;
+export default Login;
